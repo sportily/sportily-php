490a6145fd1a8a8204bdb72d67991c01b136f6d9
Craig Smith <craig@mudslidedesign.co.uk>
2015-03-06 15:23:53 +0000

    Fix typo in the core sportily Api class.

--- lib/Api.php
+++ lib/Api.php
@@ -66,7 +66,7 @@ abstract class Api {
     public static function getAccessToken() {
         if (self::$access_token === null) {
             self::$access_token = '';
-            self::$access_token = SportilyOAuth::token();
+            self::$access_token = OAuth::token();
         }
 
         return self::$access_token;
