df514bc79bb65764ee0908f6e4022b90c6181bfa
Craig Smith <craig@mudslidedesign.co.uk>
2015-10-17 10:33:45 +0100

    Added support for 'refresh_token' grant type.

--- src/OAuth.php
+++ src/OAuth.php
@@ -8,29 +8,49 @@ abstract class OAuth {
      * an auth code obtained through the user the regular authorization flow,
      * or by simply asking for an application-wide token.
      *
+     * @deprecated this method does not support the 'refresh_token' grant type,
+     * use getToken($grant_type, $data) instead
+     *
      * @param string $auth_code an auth code to exchange for an access token
      *
      * @return string a valid access token
      */
-    public static function token($auth_code = null) {
-        $grant_type = $auth_code ? 'authorization_code' : 'client_credentials';
+     public static function token($auth_code = null) {
+         $grant_type = null;
+         $data = [];
 
-        $payload = [
-            'grant_type' => $grant_type,
-            'client_id' => Api::$client_id,
-            'client_secret' => Api::$client_secret
-        ];
+         if ($auth_code) {
+             $grant_type = 'authorization_code';
+             $data['code'] = $auth_code;
+             $data['redirect_uri'] = Api::$redirect_url;
+         } else {
+             $grant_type = 'client_credentials';
+         }
 
-        if ($auth_code) {
-            $payload['grant_type'] = 'authorization_code';
-            $payload['code'] = $auth_code;
-            $payload['redirect_uri'] = Api::$redirect_url;
-        } else {
-            $payload['grant_type'] = 'client_credentials';
-        }
+         return self::getToken($grant_type, $data);
+     }
 
-        return Requester::post('oauth/token', $payload);
-    }
+     /**
+      * Retrieve an access token for the given grant_type and payload.
+      *
+      * @param string $grant_type the grant type to use
+      * @param array $data extra data to send to the oauth server
+      *
+      * @return string a valid access token
+      */
+     public static function getToken($grant_type, $data) {
+         $payload = array_merge([
+             'grant_type' => $grant_type,
+             'client_id' => Api::$client_id,
+             'client_secret' => Api::$client_secret
+         ], $data);
+
+         if ($grant_type == 'authorization_code') {
+             $payload['redirect_uri'] = Api::$redirect_url;
+         }
+
+         return Requester::post('oauth/token', $payload);
+     }
 
     /**
      * Helper function to generate the URL that the user should be sent to in
